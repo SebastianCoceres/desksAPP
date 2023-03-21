cb8c5e1921e22e1117b8c58cc78f0295de8b14e2
Sebastian Coceres <sebastian.coceres11@gmail.com>
2023-03-21 20:00:13 +0100

    setting up the ui and the api, add /decks endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import DeckModel from "./models/Decks";
+
+const PORT = 5000;
+
+var app = express();
+
+app.use(express.json())
+
+app.get('/test', (req: Request, res: Response) => {
+  res.send('Testing')
+})
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new DeckModel({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose.connect("mongodb://localhost:27017/brainstorm_app").then(() => {
+  console.log(`Listening on port ${PORT}`);
+  app.listen(PORT);
+});
